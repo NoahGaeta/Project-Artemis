5d251d63613ac7486c4a09da146f2fb7bed60681
Noah Gaeta <ngaet02@gmail.com>
2021-06-26 12:02:48 -0400

    added obs and env files

--- ofthin.py
+++ ofthin.py
@@ -7,16 +7,16 @@ HTML_RUN_FOLDER = 'runs_html'
 JSON_RUN_FOLDER = 'runs_json'
 JSON_RUN_COUNTER = 'run_counter.txt'
 
+
 def main():
     clean_html()
     initial_count = get_json_run_count()
     for i in range(0, 5):
         game = make('hungry_geese')
-        game.run([agent, agent])
+        game.run(['greedy', 'random', 'greedy', 'random'])
         write_game_replay(game, i)
         write_data(game, initial_count + i)
         set_count_file(initial_count + i)
-
     return 0
 
 
@@ -28,6 +28,7 @@ def get_json_run_count():
         count = int(counter_file.readline())
         print(count)
     return count
+
 
 def set_count_file(count):
     counter_file_path = os.path.join(JSON_RUN_FOLDER, JSON_RUN_COUNTER)
@@ -52,26 +53,6 @@ def write_game_replay(game, idx):
     html_file = open(os.path.join(HTML_RUN_FOLDER, 'run_{0}.html'.format(idx)), 'w')
     html_file.write(html_string)
 
-
-def agent(obs_dict, config_dict):
-    """This agent always moves toward observation.food[0] but does not take advantage of board wrapping"""
-    print(obs_dict)
-    observation = Observation(obs_dict)
-    configuration = Configuration(config_dict)
-    player_index = observation.index
-    player_goose = observation.geese[player_index]
-    player_head = player_goose[0]
-    player_row, player_column = row_col(player_head, configuration.columns)
-    food = observation.food[0]
-    food_row, food_column = row_col(food, configuration.columns)
-
-    if food_row > player_row:
-        return Action.SOUTH.name
-    if food_row < player_row:
-        return Action.NORTH.name
-    if food_column > player_column:
-        return Action.EAST.name
-    return Action.WEST.name
 
 
 if __name__ == '__main__':
